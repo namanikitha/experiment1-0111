244d7aecfac4f98200472dc947897f3db7467903
namanikitha <103954387+namanikitha@users.noreply.github.com>
2023-12-17 16:25:11 +0530

    Create task-1.py

---
+++ task-1.py
@@ -0,0 +1,35 @@
+# p1 hard coded variables keyboard input
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# def quadratic_solution(a, b, c, x):
+#       y = a * x**2 + b * x + c
+#       return y
+# def plot_temperature_over_time(a, b, c, time_range):
+#       """
+#       Plot temperature over time using a quadratic function.
+#       """
+#       time = np.array(time_range)
+#       temperature = quadratic_solution(a, b, c, time)
+
+#       plt.plot(time, temperature)
+#       plt.title("Temperature Over Time")
+#       plt.xlabel("Time")
+#       plt.ylabel("Temperature")
+#       plt.grid(True)
+#       plt.show()
+
+# def main():
+#       print("Weather Modeling using Quadratic Solution")
+
+#       # Hard-coded coefficients
+#       a_hardcoded=int(input("enter value of a : "))
+#       b_hardcoded=int(input("enter value of b : "))
+#       c_hardcoded=int(input("enter value of c : "))
+#       time_range = np.linspace(0, 10, 100)  # Adjust the time range as needed
+
+#       # Plot temperature over time using the quadratic function
+#       plot_temperature_over_time(a_hardcoded, b_hardcoded, c_hardcoded, time_range)
+
+# if _name_ == "_main_":
+#       main()
